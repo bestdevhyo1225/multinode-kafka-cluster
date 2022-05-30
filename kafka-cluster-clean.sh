5008eb935a90f5aece79fb6c190d2784621ae6dc
janghyoseog <bestdevhyo1225@gmail.com>
2022-05-30 16:24:55 +0900

    style: 오타 수정

--- kafka-cluster-clean.sh
+++ kafka-cluster-clean.sh
@@ -1,4 +1,4 @@
-docker stop kafka1 kafka2 kafak3 zookeeper1 zookeeper2 zookeeper3 kafdrop
-docker rm kafka1 kafka2 kafak3 zookeeper1 zookeeper2 zookeeper3 kafdrop
+docker stop kafka1 kafka2 kafka3 zookeeper1 zookeeper2 zookeeper3 kafdrop
+docker rm kafka1 kafka2 kafka3 zookeeper1 zookeeper2 zookeeper3 kafdrop
 docker network rm kafka-cluster-net
 docker volume prune -f
