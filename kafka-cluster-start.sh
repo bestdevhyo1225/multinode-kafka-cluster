a346107befce137ae3f2d164db27400d6cdca468
janghyoseog <bestdevhyo1225@gmail.com>
2022-05-30 17:34:23 +0900

    chore: sleep 제거

--- kafka-cluster-start.sh
+++ kafka-cluster-start.sh
@@ -3,12 +3,8 @@
 # zookeeper1, kafka1, kafdrop 도커 컨테이너 실행
 docker-compose -f node1/docker-compose.yml up -d
 
-sleep 1000
-
 # zookeeper2, kafka2 도커 컨터이너 실행
 docker-compose -f node2/docker-compose.yml up -d
-
-sleep 1000
 
 # zookeeper3, kafka3 도커 컨터이너 실행
 docker-compose -f node3/docker-compose.yml up -d
