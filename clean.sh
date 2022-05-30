63a4c7dbd6df4a56aac0d4892eba8e33376d7c96
janghyoseog <bestdevhyo1225@gmail.com>
2022-05-30 15:41:00 +0900

    chore: 도커 정리 쉘 스크립트 추가

---
+++ clean.sh
@@ -0,0 +1,3 @@
+docker stop kafka1 kafka2 kafak3 zookeeper1 zookeeper2 zookeeper3 kafdrop
+docker rm kafka1 kafka2 kafak3 zookeeper1 zookeeper2 zookeeper3 kafdrop
+docker network rm kafka-cluster-net
