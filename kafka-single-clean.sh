7a355cd7f72b56403e87cd5471ef07dc73657efb
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-05 02:10:06 +0900

    chore: single 노드 설정

--- kafka-single-clean.sh
+++ kafka-single-clean.sh
@@ -1,4 +1,7 @@
-docker stop kafka1 zookeeper1 kafka_manager
-docker rm kafka1 zookeeper1 kafka_manager
+docker stop kafka1 zookeeper1 kafdrop kafka_manager
+docker rm kafka1 zookeeper1 kafdrop kafka_manager
 docker network rm node1_kafka-net
 docker volume prune -f
+
+# Data 파일 삭제
+rm -rf node1/kafka-data node1/kafka-varlog node1/zookeeper-data node1/zookeeper-varlog
