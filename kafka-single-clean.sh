a32f84347637fb1649619a333cd6527ded02294f
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-05 02:10:24 +0900

    chore: 일부 명령어 주석처리

--- kafka-single-clean.sh
+++ kafka-single-clean.sh
@@ -4,4 +4,4 @@ docker network rm node1_kafka-net
 docker volume prune -f
 
 # Data 파일 삭제
-rm -rf node1/kafka-data node1/kafka-varlog node1/zookeeper-data node1/zookeeper-varlog
+#rm -rf node1/kafka-data node1/kafka-varlog node1/zookeeper-data node1/zookeeper-varlog
