33eaf26e3bf2783f645368ada9911ea257a84421
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-23 10:59:52 +0900

    chore: 데이터 파일도 같이 삭제 되도록 명령어 활성화

--- kafka-cluster-clean.sh
+++ kafka-cluster-clean.sh
@@ -7,6 +7,6 @@ docker network rm kafka-cluster-net
 docker volume prune -f
 
 # Data 파일 삭제
-#rm -rf node1/kafka-data node1/kafka-varlog node1/zookeeper-data node1/zookeeper-varlog
-#rm -rf node2/kafka-data node2/kafka-varlog node2/zookeeper-data node2/zookeeper-varlog
-#rm -rf node3/kafka-data node3/kafka-varlog node3/zookeeper-data node3/zookeeper-varlog
+rm -rf node1/kafka-data node1/kafka-varlog node1/zookeeper-data node1/zookeeper-varlog
+rm -rf node2/kafka-data node2/kafka-varlog node2/zookeeper-data node2/zookeeper-varlog
+rm -rf node3/kafka-data node3/kafka-varlog node3/zookeeper-data node3/zookeeper-varlog
